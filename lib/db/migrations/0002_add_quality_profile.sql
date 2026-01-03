2060fb16f51fd22df65b56fc873f4cef8fbc6d16
jordank1977 <jordank1977@proton.me>
2026-01-02 23:03:19 -0800

    Remove unsupported DROP COLUMN from migration 0002
    SQLite has limited ALTER TABLE support and DROP COLUMN
    causes drizzle-kit migrate to fail. Keeping the type column
    doesn't hurt anything.

--- lib/db/migrations/0002_add_quality_profile.sql
+++ lib/db/migrations/0002_add_quality_profile.sql
@@ -1,5 +1,2 @@
 -- Add quality_profile_id column to requests table
 ALTER TABLE requests ADD COLUMN quality_profile_id INTEGER NOT NULL DEFAULT 1;
-
--- Drop the old type column
-ALTER TABLE requests DROP COLUMN type;
